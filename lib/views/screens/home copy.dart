b5375a84a50eb0c73fc23a9b63161c63f4dd6f14
enrahsn <enrahsn@gmail.com>
2024-04-16 13:31:36 +0200

    GetBuilder

--- lib/views/screens/home copy.dart
+++ lib/views/screens/home copy.dart
@@ -1,62 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import '../../controller/home_ctrl.dart';
-// ignore: must_be_immutable
-class HomePage extends StatelessWidget {
-      HomePage({super.key});
-
-
-int counter =0;
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Getx App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-         GetBuilder<HomeCtrl>(
-          init: HomeCtrl(),
-          builder: (controller)=> Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(20),
-                child: IconButton(
-                    onPressed: () {
-                      controller.increment();
-                      },
-                    icon: const Icon(
-                      Icons.add,
-                      size: 33,
-                    ))),
-              Text(
-                "${controller.counter}",
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: IconButton(
-                    onPressed: () {
-                      controller.decrement() ;
-                    },
-                    icon: const Icon(
-                      Icons.remove,
-                      size: 33,
-                    )),
-              ),
-            ],
-          ),)
-        ],
-      ),
-    );
-  }
-}
-
-/*
 class HomePage extends StatefulWidget {
   const HomePage({super.key, });
 
@@ -117,4 +60,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-*/
--- lib/views/screens/index.dart
+++ lib/views/screens/index.dart
@@ -1,4 +1,5 @@
 export 'edit_note_view.dart';
+export 'home copy.dart';
 export 'home.dart';
 export 'notes_view.dart';
 
